6b2752f1cf5a16c0dec5b15450cec5327a78de36
vicktorl <v.lenam01@ufromail.cl>
2023-05-22 14:05:06 -0400

    se sube desafio 1

---
+++ src/test/java/ufro/demo/CarApplicationTests.java
@@ -0,0 +1,13 @@
+package ufro.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
